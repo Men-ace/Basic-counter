a4ed4b26101b81c2eaa5152ae8da853e24916dff
Men-ace <chhettrisailesh1@gmail.com>
2025-02-17 19:13:32 +1100

    written logic for it

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,8 @@ import { useState } from 'react';
 function App() {
   // logic
 const [counter, setCounter] = useState(10)
-const[num1] = useState(0)
-const[num2] = useState(0)
-const [result] = useState(null)
+
+
 
   const increase = ()=>{
     setCounter(counter+1)
@@ -20,17 +19,14 @@ const [result] = useState(null)
  
 
      
- console.log(result)
+
   console.log(counter)
   return (
     <>
     <div>{counter}</div>
     <div onClick={increase}>+</div>
     <div onClick={decrease}>-</div>
-    <input type="number" value={num1} />
-    <input type="number" value={num2} />
-    
-    <p>Result: {num1 * num2}</p>
+   
     
     </>
   )  
